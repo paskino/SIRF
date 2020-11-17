8dbf699ec5e83beffef0e1136a88b28a444a56de
Edoardo Pasca <edo.paskino@gmail.com>
2020-11-17 21:42:59 +0000

    updated for running on SCARF

--- examples/Python/PETMR/ungated_spdhg.sh
+++ examples/Python/PETMR/ungated_spdhg.sh
@@ -1,33 +1,50 @@
 #! /bin/bash
 
-loc_data=/home/sirfuser/data/cardiac_resp
-loc_algo=~/devel/claire/SIRF/examples/Python/PETMR
-loc_reco=~/devel/claire/cluster_test/recons
-loc_param=~/devel/claire/cluster_test/params
+# base directory
+mcir_dir=/home/vol05/scarf595/MCIR
+work_dir=/work3/cse/synerbi/
+loc_data=${work_dir}/cardiac_resp
+loc_algo=${mcir_dir}/SIRF/examples/Python/PETMR
+
+base_result=${work_dir}/results
+run_name=ungated_spdhg_test
+loc_reco=${base_result}/${run_name}/recons
+loc_param=${base_result}/${run_name}/params
                        
+# create the run directory and get in there
+mkdir -p ${base_result}/${run_name}
+cd ${base_result}/${run_name}
 
-python $loc_algo/PET_MCIR_PD.py             \
--o ungated_spdhg                              \
---algorithm=spdhg                            \
--r FGP_TV                                   \
---outpath=$loc_reco                        \
---param_path=$loc_param                    \
--e 500                                     \
---update_obj_fn_interval=480                  \
---descriptive_fname                         \
--v 0                                        \
--S "$loc_data/pet/STIRungated/sinospan11_f1g1d0b0.hs"   \
--R "$loc_data/pet/STIRungated/total_background.hs"      \
--n "$loc_data/pet/NORM.n.hdr"           \
--a "$loc_data/pet/MU.hv"             \
---nifti                                   \
---alpha=5.0                                \
---precond                                  \
---dxdy=3.12117                             \
---nxny=180                                 \
---numSubsets=48                             \
---numThreads=27                             
+# run configuration
+#epochs=500
+#update_interval=480
+      
+# test configuration
+epochs=2
+update_interval=48      
 
+cp ${loc_algo}/PET_MCIR_PD.py ${loc_reco}
+cd ${loc_reco}
 
-                                                          
-                                    
+python PET_MCIR_PD.py                                 \
+-o ungated_spdhg                                      \
+--algorithm=spdhg                                     \
+-r FGP_TV                                             \
+--outpath=$loc_reco                                   \
+--param_path=$loc_param                               \
+-e ${epochs}                                           \
+--update_obj_fn_interval=${update_interval}           \
+--descriptive_fname                                   \
+-v 0                                                  \
+-S "$loc_data/pet/STIRungated/sinospan11_f1g1d0b0.hs" \
+-R "$loc_data/pet/STIRungated/total_background.hs"    \
+-n "$loc_data/pet/NORM.n.hdr"                         \
+-a "$loc_data/pet/MU.hv"                              \
+--nifti                                               \
+--alpha=5.0                                           \
+--precond                                             \
+--dxdy=3.12117                                        \
+--nxny=180                                            \
+--numSubsets=48                                       \
+--numThreads=48
+
