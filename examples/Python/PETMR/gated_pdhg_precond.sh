4a4131ab2fe2ba926823c04fb4e2853ec5b43e98
something@gmail.com <something@gmail.com>
2020-11-23 12:57:17 +0000

    Add launch file

---
+++ examples/Python/PETMR/gated_pdhg_precond.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+loc_data=/home/sirfuser/data/cardiac_resp
+loc_algo=~/devel/claire/SIRF/examples/Python/PETMR
+loc_reco=~/devel/claire/gated/recons
+loc_param=~/devel/claire/gated/params
+                       
+
+python $loc_algo/PET_MCIR_PD.py             \
+-o gated_pdhg_precond                        \
+--algorithm=pdhg                            \
+-r FGP_TV                                   \
+--outpath=$loc_reco                        \
+--param_path=$loc_param                    \
+-e 2000                                     \
+--update_obj_fn_interval=10                \
+--descriptive_fname                         \
+-v 0                                        \
+-S "$loc_data/pet/EM_g*.hs"                  \
+-R "$loc_data/pet/total_background_g*.hs"      \
+-n "$loc_data/pet/NORM.n.hdr"               \
+-a "$loc_data/pet/MU_g*.nii"                  \
+-T "$loc_data/pet/transf_g*.nii"            \
+-t def                                    \
+--nifti                                   \
+--alpha=5.0                                \
+--dxdy=3.12117                             \
+--nxny=180                                 \
+--precond                                   \
+--numThreads=27
+
