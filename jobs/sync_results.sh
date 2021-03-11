54b4bfb6f5ce1fa4ed2e82bf021228299ff75b9f
Edoardo Pasca <edo.paskino@gmail.com>
2021-03-11 13:57:00 +0000

    updates march2021

--- jobs/sync_results.sh
+++ jobs/sync_results.sh
@@ -4,45 +4,66 @@ work_dir=/work3/cse/synerbi
 base_result=${work_dir}/results
 ##############    RUN NAMES    ################
 run_names=(\
-"run_gated_view_gates" "ungated_pdhg" "ungated_spdhg" "gated_pdhg" "gated_gates" "ungated_pdhg_5000" \
+# "run_gated_view_gates" "ungated_pdhg" "ungated_spdhg" "gated_pdhg" "gated_gates" "ungated_pdhg_5000" \
 # "alpha_0.1_gated_gates" "alpha_0.5_gated_gates" "alpha_1_gated_gates"   "alpha_5_gated_gates"  "alpha_10_gated_gates"\
 #   "alpha_15_gated_gates"  "alpha_20_gated_gates"  "alpha_25_gated_gates"  "alpha_30_gated_gates"  \
 #   "alpha_35_gated_gates"  
-  "noprecond_alpha_5_gated_gates" 
-  "noprecond_alpha_5_gated_pdhg" 
-  "precond_alpha_5_gated_pdhg"
-  "rebin_initial_rescaled_gamma_1.0_noprecond_alpha_1.0_gated_gates"
-  "rebin_noinitial_rescaled_gamma_1.0_noprecond_alpha_1.0_gated_gates"
-  "rescaled_gamma_1.0_alpha_1.0_ungated_pdhg"
-  "rescaled_gamma_1.0_alpha_5.0_ungated_pdhg"
-  "rescaled_gamma_1.0_noprecond_alpha_1.0_gated_pdhg"
-  "rescaled_gamma_1.0_noprecond_alpha_5.0_gated_pdhg"
-  "rescaled_gamma_1.0_notrans_alpha_1.0_gated_pdhg"
-  "rescaled_gamma_1.0_notrans_alpha_5.0_gated_pdhg"
-  "rescaled_gamma_2.8284_noprecond_alpha_0.1_gated_pdhg"
-  "rescaled_gamma_2.8284_noprecond_alpha_1.0_gated_pdhg"
-  "rescaled_gamma_2.8284_noprecond_alpha_5_gated_pdhg"
-  "rescaled_gamma_2.8284_notrans_alpha_0.1_gated_pdhg"
-  "rescaled_gamma_2.8284_notrans_alpha_1.0_gated_pdhg"
-  "rescaled_gamma_2.8284_notrans_alpha_5.0_gated_pdhg"
-  "rescaled_gamma_2.8284_notrans_alpha_5_gated_pdhg"
-  "rescaled_noprecond_alpha_0.1_gated_gates"
-  "rescaled_noprecond_alpha_1.0_gated_gates"
-  "rescaled_noprecond_alpha_5_gated_gates"
+  # "noprecond_alpha_5_gated_gates" 
+  # "noprecond_alpha_5_gated_pdhg" 
+  # "precond_alpha_5_gated_pdhg"
+  # "rebin_initial_rescaled_gamma_1.0_noprecond_alpha_1.0_gated_gates"
+  # "rebin_noinitial_rescaled_gamma_1.0_noprecond_alpha_1.0_gated_gates"
+  # "rescaled_gamma_1.0_alpha_1.0_ungated_pdhg"
+  # "rescaled_gamma_1.0_alpha_5.0_ungated_pdhg"
+  # "rescaled_gamma_1.0_noprecond_alpha_1.0_gated_pdhg"
+  # "rescaled_gamma_1.0_noprecond_alpha_5.0_gated_pdhg"
+  # "rescaled_gamma_1.0_notrans_alpha_1.0_gated_pdhg"
+  # "rescaled_gamma_1.0_notrans_alpha_5.0_gated_pdhg"
+  # "rescaled_gamma_2.8284_noprecond_alpha_0.1_gated_pdhg"
+  # "rescaled_gamma_2.8284_noprecond_alpha_1.0_gated_pdhg"
+  # "rescaled_gamma_2.8284_noprecond_alpha_5_gated_pdhg"
+  # "rescaled_gamma_2.8284_notrans_alpha_0.1_gated_pdhg"
+  # "rescaled_gamma_2.8284_notrans_alpha_1.0_gated_pdhg"
+  # "rescaled_gamma_2.8284_notrans_alpha_5.0_gated_pdhg"
+  # "rescaled_gamma_2.8284_notrans_alpha_5_gated_pdhg"
+  # "rescaled_noprecond_alpha_0.1_gated_gates"
+  # "rescaled_noprecond_alpha_1.0_gated_gates"
+  # "rescaled_noprecond_alpha_5_gated_gates"
+  # "new_motion_rescaled_gamma_1.0_noprecond_alpha_1.0_gated_pdhg"
+  # "new_motion_rescaled_gamma_{1.0}_noprecond_alpha_1.0_gated_gates"
+  # "new_motion_rebin_noinitial_rescaled_gamma_1.0_noprecond_alpha_1.0_gated_gates"
+  # "new_motion_rebin_rescaled_gamma_2.8284_noprecond_alpha_1.0_gated_pdhg"
+  # "new_motion_rebin_rescaled_gamma_1.0_noprecond_alpha_1.0_gated_pdhg"
+  # "notrans_rebin_rescaled_gamma_2.8284_noprecond_alpha_1.0_gated_pdhg"
+  # "notrans_rebin_rescaled_gamma_1.0_noprecond_alpha_1.0_gated_pdhg"
+  # "new_motion_rebin_noinitial_rescaled_gamma_1.0_noprecond_alpha_1.0_gated_gates"
+  # "rebin_rescaled_gamma_1.0_alpha_1.0_ungated_pdhg"
+  # "new_motion_rebin_rescaled_gamma_1.0_noprecond_alpha_5.0_gated_pdhg"
+  "pdhg"
+
+
 )
 
 current_dir=`pwd`
 for run_name in ${run_names[@]}
 do 
+  echo ""
+  echo ""
+  echo "######################################################################"
+  echo ""
   echo "should rsync ${base_result}/${run_name}"
+  echo ""
+  echo "######################################################################"
+  
   cd ${base_result}/${run_name}
   echo `pwd`
   # sync reconstructions
   rsync -avz --exclude "tmp_*.hs" --exclude "tmp_*.s" \
      -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" \
      --progress . \
-     sirfuser@130.246.214.97:~/data/cardiac_resp/pet/recons/${run_name}
+    sirfuser@130.246.214.97:/mnt/data/cardiac_resp/pet/recons/${run_name}
   
+  # sirfuser@130.246.214.97:/mnt/data/cardiac_resp/pet/recons/pdhg
   # # sync parameters
   # rsync -avz -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" \
   # --progress params \
